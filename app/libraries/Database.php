d07a53d8b65c7a680e1312377d36b04680845f1c
Fatema Tawfik <fatma.tawfik@addictaco.com>
2022-09-05 20:14:28 +0200

    edit db file

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -7,10 +7,10 @@
 */
 
 class Database {
-    private $host = 'localhost';
-    private $user = 'root';
-    private $pass = '';
-    private $dbname = 'products';
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
 
     private $dbh;
     private $stmt;
