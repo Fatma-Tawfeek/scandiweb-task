e81667c9d0f3bd8ea82d76895bb57621974a04a1
Fatema Tawfik <fatma.tawfik@addictaco.com>
2022-09-14 12:28:58 +0200

    edit config

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
 // DB params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'products');
+define('DB_HOST', 'ec2-54-82-205-3.compute-1.amazonaws.com');
+define('DB_USER', 'lnntrdlnkdoqmn');
+define('DB_PASS', '7d4e691001361618733d4563ba753296b7abe500d1e92f067b8b9d6c8e3054f2');
+define('DB_NAME', 'dcadncedpg33r3');
 
 // App Root
 define('APPROOT',dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/scandiweb-task');
+define('URLROOT', 'https://guarded-eyrie-26648.herokuapp.com/');
 // Site Name
 define('SITENAME', 'Scandiweb Task');
